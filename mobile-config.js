d98cc5fd341a63a13f34e633293ad60775b8c7f6
Marcus Tyler-Moore <mtmoore1972@gmail.com>
2016-07-21 00:39:58 +0100

    getLocation, auto start location and more types added

--- mobile-config.js
+++ mobile-config.js
@@ -7,3 +7,5 @@ App.accessRule('https://*.googleapis.com/*');
 App.accessRule('https://*.google.com/*');
 App.accessRule('https://*.gstatic.com/*');
 App.accessRule('http://*.emptymoore.com/*');
+App.accessRule('http://*', {type: 'intent'});
+App.accessRule('https://*', {type: 'intent'});
